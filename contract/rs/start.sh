1fc77a17d2faae744dd59ae1edcd5ee934d453a4
CatScan-Script <mariroth60@gmail.com>
2025-05-20 01:35:57 +0300

    .

---
+++ contract/rs/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
+. "$HOME/.asdf/asdf.sh"
+
+# Install Node.js
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || true
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf install nodejs latest
+asdf global nodejs latest
+
+# Install Scarb
+asdf plugin add scarb https://github.com/sdroege/asdf-scarb.git || true
+asdf install scarb latest
+asdf global scarb latest
+
+# Install Node app dependencies
+npm install
