e8d5766435cc3e9d0ed11458af6bbf53e1e63908
CatScan-Script <mariroth60@gmail.com>
2025-05-20 15:10:35 +0300

    .

--- contract/rs/server.js
+++ contract/rs/server.js
@@ -4,22 +4,38 @@ const app = express();
 
 app.use(express.json());
 
-// ✅ Hello World route
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-// ✅ Scarb version route
+
 app.get('/version', ( req,res) => {
-exec('mise exec scarb@2.8.4 --command "scarb build"', (err, stdout, stderr) => {
-  if (err) {
-    return res.status(500).json({ error: stderr });
-  }
-  res.json({ version: stdout.trim() });
+  exec('mise exec scarb@2.8.4 --command "scarb --version"', (err, stdout, stderr) => {
+    if (err) {
+      return res.status(500).json({ error: stderr });
+    }
+    res.json({ version: stdout.trim() });
+  });
 });
 
+app.get('/mp', ( req,res) => {
+  exec('mise exec scarb@2.8.4 --command "scarb manifest-path', (err, stdout, stderr) => {
+    if (err) {
+      return res.status(500).json({ error: stderr });
+    }
+    res.json({ version: stdout.trim() });
+  });
 });
 
+app.get('/md', ( req,res) => {
+  exec('mise exec scarb@2.8.4 --command "scarb metadata', (err, stdout, stderr) => {
+    if (err) {
+      return res.status(500).json({ error: stderr });
+    }
+    res.json({ version: stdout.trim() });
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
