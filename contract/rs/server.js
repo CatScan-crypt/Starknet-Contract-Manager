3610439d569c5494d4e9adaba3b0e16ba8279552
CatScan-Script <mariroth60@gmail.com>
2025-05-22 21:35:50 +0300

    Added routes dir.

--- contract/rs/server.js
+++ contract/rs/server.js
@@ -2,17 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
-const rootRoute = require('./rootRoute');
-const sendDocRoute = require('./sendDocRoute');
-const statusRoute = require('./statusRoute');
-const downloadRoute = require('./downloadRoute');
-const installerRoute = require('./installerRoute');
+const rootRoute = require('./routes/rootRoute');
+const sendDocRoute = require('./routes/sendDocRoute');
+const statusRoute = require('./routes/statusRoute');
+const downloadRoute = require('./routes/downloadRoute');
 
 app.use('/', rootRoute);
 app.use('/', sendDocRoute);
 app.use('/', statusRoute);
 app.use('/', downloadRoute);
-app.use('/', installerRoute);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
