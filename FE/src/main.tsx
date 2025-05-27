a6711cbfc92c1dae295fa55427e5384e2cb8948e
catscan-crypt <mariroth60@gmail.com>
2025-05-27 21:35:19 +0300

    Adding Vercel analytics.

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from '@vercel/analytics/react';
 
 import { StarknetProvider } from "./StarknetConfig";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StarknetProvider>
       <App />
+      <Analytics />
     </StarknetProvider>
   </StrictMode>,
 )
